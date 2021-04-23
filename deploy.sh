2d3d00c8bf5183b2471eb945b9ced4e9e1584c4f
Skyery <ru042l41j4@gmail.com>
2021-04-23 14:25:57 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# abort on errors
 set -e
 
-# 构建
+# build
 npm run build
 
-# cd 到构建输出的目录下 
+# navigate into the build output directory
 cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:Skyery/Skyery.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Skyery/Practice-ShoppingCart.git master:gh-pages
 
 cd -
